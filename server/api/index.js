f0bc4f864bcafd8c8d566b50698e70dd21ba26f2
Molly Seeley <mseeley89@gmail.com>
2018-12-03 10:33:18 -0800

    404 error handling added

--- server/api/index.js
+++ server/api/index.js
@@ -2,4 +2,10 @@ const router = require(express).Router()
 //To break out/add more routes:
 //router.use('/subrouteUri', require('folderpath'))
 
+router.use((req, res, next) => { //passes all errors to server/index.js error handler
+  const err = new Error('Not Found')
+  res.status(404)
+  next(err)
+})
+
 module.exports = router
