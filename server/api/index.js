c179fbdfa23439d84b63709892203c0ceb0fd0e0
Molly Seeley <mseeley89@gmail.com>
2018-12-03 10:29:55 -0800

    added api folder and format to break out routes

---
+++ server/api/index.js
@@ -0,0 +1,5 @@
+const router = require(express).Router()
+//To break out/add more routes:
+//router.use('/subrouteUri', require('folderpath'))
+
+module.exports = router
--- server/index.js
+++ server/index.js
@@ -5,3 +5,8 @@ const path = require('path') //build-it
 app.use('volleyball') //logging middleware for requests and response
 app.use(express.static(path.join(__dirname, '..', 'public'))) // forward slash only works on linux and windows.
 
+app.use(express.json()) //json parsing
+app.use(express.urlencoded({ extended: true })) //url encoding parsing
+
+app.use('/api', require('./api')) //api url prefix for routes from api folder
+
