256eee34527fbd62b394d82e25c401a42474b690
Molly Seeley <mseeley89@gmail.com>
2019-01-13 15:21:23 -0800

    user model password encription setup

--- server/db/user.js
+++ server/db/user.js
@@ -1,7 +1,62 @@
+const Sequelize = require('sequelize')
 const { db } = require('./database')
+const crypto = require('crypto')
 
 const User = db.defint('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: false
+    }
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+  password: {
+    type: Sequelize.STRING,
+    get() {
+      return this.getDataValue('password')
+    }
+  },
+  googleId: {
+    type: Sequelize.STRING,
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  }
 
 })
 
 module.exports = User
+
+User.prototype.password = function (submittedPass) {
+  return User.encryptPassword(submittedPass, this.salt() === this.password())
+}
+
+User.generateSalt = function () {
+  return crypto.randomBytes(16).toString('base64')
+}
+
+User.encryptPassword = function () {
+  return crypto
+    .createHash('RSA-SHA256')
+    .update(plainText)
+    .update(salt)
+    .digest('hex')
+}
+
+const setSaltAndPassword = user => {
+  if (user.changed('password')) {
+    user.salt = User.generateSalt()
+    user.password = User.encryptPassword(user.password(), user.salt())
+  }
+}
+
+User.beforeCreate(setSaltAndPassword)
+User.beforeUpdate(setSaltAndPassword)
