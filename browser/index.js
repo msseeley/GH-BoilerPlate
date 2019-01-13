b79dd4589da231f84b534a0910a66c09a723fca1
Molly Seeley <mseeley89@gmail.com>
2019-01-13 13:24:09 -0800

    added import store in browser/index.js

--- browser/index.js
+++ browser/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'redux'
+import store from './store'
 import './index.css'
+
 render(
-  <Provider>
+  <Provider store={store}>
     <h1>WELCOME TO GH-BOILERPLATE!</h1>
   </Provider>,
   document.getElementById('app'))
