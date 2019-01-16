09402923e0ea9e2166a149176af222de34f9ac6d
Molly Seeley <mseeley89@gmail.com>
2019-01-15 19:20:16 -0800

    refactoring: local.js file absorbed by index.js in auth

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,19 +1,73 @@
 const router = require('express').Router()
 
-//fetches the logged in user
+const { User } = require("../../db")
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    if (req.body.isAdmin === true && !req.user.isAdmin) { res.send("You don not have permissions to set Admin") }
+    else {
+      //ends up being signup or login bc of findOrCreate
+      const newUser = await User.create(req.body)
+      req.login(newUser, error => (error ? next(error) : res.json(newUser)))
+    }
+  }
+  catch (error) {
+    if (error.name === "SequelizeUniqueConstraintError") {
+      res.status(401).send('User already exists')
+    }
+    else { next(error) }
+  }
+})
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const foundUser = await User.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+    if (!foundUser) {
+      console.log('No such user found')
+      res.status(401).send('No such user found, perhaps you input an incorrect username and/or password?')
+    }
+    else if (!foundUser.checkPassword(req.body.password)) {
+      res.status(401).send(`Incorrect password for ${req.body.email}`)
+    }
+    else {
+      req.login(foundUser, error => (error ? next(error) : res.json(foundUser)))
+    }
+
+  }
+  catch (error) {
+    next(error)
+  }
+})
+
+router.post('/logout', (req, res, next) => {
+  req.logout()
+  req.session.destroy()
+  res.redirect('/')
+})
+
 router.get('/me', (req, res, next) => {
   res.json(req.user || {})
 })
 
-router.delete('/', (req, res, next) => {
-  req.logout()
-  req.session.destroy(error => {
-    if (error) { return next(error) }
-    res.status(204).end('successfully logged out')
-  })
+//Put request /auth/local means changing current user, updating the user on the session
+router.put('/', async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } })
+    if (!user) { res.status(401).send("User not found") }
+    else if (!user.checkPassword(req.body.password)) { res.send("Incorrect Password") }
+    else {
+      req.login(user, error => (error ? next(error) : res.json(user)))
+    }
+  }
+  catch (error) {
+    next(error)
+  }
 })
 
-router.use('/local', require('./local'))
 router.use('/google', require('./google'))
 
 router.use((req, res, next) => {
